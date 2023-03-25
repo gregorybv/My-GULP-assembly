47522e30ca91d609e277d77a4f9caeb22d35edaf
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-25 17:18:39 +0300

    new edits

--- src/js/functions/modalWindow.js
+++ src/js/functions/modalWindow.js
@@ -1,11 +1,11 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+const modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+const btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
@@ -23,3 +23,6 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+/* ========================================== */
+
